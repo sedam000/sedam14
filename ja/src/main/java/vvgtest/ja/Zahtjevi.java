e77fea8acb62f0eae46aabe25fd77e1a178c4f73
admin <>
2015-11-10 01:25:42 +0100

    fsfsf {/ja/src/main/java/vvgtest/ja/Zahtjevi.java}

--- ja/src/main/java/vvgtest/ja/Zahtjevi.java
+++ ja/src/main/java/vvgtest/ja/Zahtjevi.java
@@ -16,14 +16,20 @@ public class Zahtjevi implements java.io.Serializable
    @org.kie.api.definition.type.Label("Kratki opis zahtjeva")
    private java.lang.String opis_zahtjeva;
 
-   @org.kie.api.definition.type.Label("Status zahtjeva")
-   private java.lang.String id_statusa;
+   @org.kie.api.definition.type.Label("status zahtjeva")
+   private Long id_statusa;
 
    @org.kie.api.definition.type.Label("Opis poslovnih pravila")
    private java.lang.String korisnicka_specifikacija;
 
    @org.kie.api.definition.type.Label(value = "Narucitelj")
-   private java.lang.String narucuteljID;
+   private java.lang.String narucitelj;
+
+   @org.kie.api.definition.type.Label(value = "kontakt_osoba")
+   private java.lang.String kontakt_osoba;
+
+   @org.kie.api.definition.type.Label(value = "korisnik")
+   private java.lang.Long id_korisnika;
 
    public Zahtjevi()
    {
@@ -59,16 +65,6 @@ public class Zahtjevi implements java.io.Serializable
       this.opis_zahtjeva = opis_zahtjeva;
    }
 
-   public java.lang.String getId_statusa()
-   {
-      return this.id_statusa;
-   }
-
-   public void setId_statusa(java.lang.String id_statusa)
-   {
-      this.id_statusa = id_statusa;
-   }
-
    public java.lang.String getKorisnicka_specifikacija()
    {
       return this.korisnicka_specifikacija;
@@ -80,26 +76,59 @@ public class Zahtjevi implements java.io.Serializable
       this.korisnicka_specifikacija = korisnicka_specifikacija;
    }
 
-   public java.lang.String getNarucuteljID()
+   public java.lang.String getNarucitelj()
    {
-      return this.narucuteljID;
+      return this.narucitelj;
    }
 
-   public void setNarucuteljID(java.lang.String narucuteljID)
+   public void setNarucitelj(java.lang.String narucitelj)
    {
-      this.narucuteljID = narucuteljID;
+      this.narucitelj = narucitelj;
+   }
+
+   public java.lang.Long getId_statusa()
+   {
+      return this.id_statusa;
+   }
+
+   public void setId_statusa(java.lang.Long id_statusa)
+   {
+      this.id_statusa = id_statusa;
+   }
+
+   public java.lang.String getKontakt_osoba()
+   {
+      return this.kontakt_osoba;
+   }
+
+   public void setKontakt_osoba(java.lang.String kontakt_osoba)
+   {
+      this.kontakt_osoba = kontakt_osoba;
+   }
+
+   public java.lang.Long getId_korisnika()
+   {
+      return this.id_korisnika;
+   }
+
+   public void setId_korisnika(java.lang.Long id_korisnika)
+   {
+      this.id_korisnika = id_korisnika;
    }
 
    public Zahtjevi(java.lang.Long id_zahtjeva, java.lang.String naziv_zahtjeva,
-         java.lang.String opis_zahtjeva, java.lang.String id_statusa,
-         java.lang.String korisnicka_specifikacija, java.lang.String narucuteljID)
+         java.lang.String opis_zahtjeva, java.lang.Long id_statusa,
+         java.lang.String korisnicka_specifikacija, java.lang.String narucitelj,
+         java.lang.String kontakt_osoba, java.lang.Long id_korisnika)
    {
       this.id_zahtjeva = id_zahtjeva;
       this.naziv_zahtjeva = naziv_zahtjeva;
       this.opis_zahtjeva = opis_zahtjeva;
       this.id_statusa = id_statusa;
       this.korisnicka_specifikacija = korisnicka_specifikacija;
-      this.narucuteljID = narucuteljID;
+      this.narucitelj = narucitelj;
+      this.kontakt_osoba = kontakt_osoba;
+      this.id_korisnika = id_korisnika;
    }
 
 }
