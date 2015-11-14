b96ea100a465ea4bfd16e2efba631a40a178868c
admin <>
2015-11-14 14:50:18 +0100

    gdgdg {/ja/src/main/java/vvgtest/ja/testovi_prihvacanja.java}

--- ja/src/main/java/vvgtest/ja/testovi_prihvacanja.java
+++ ja/src/main/java/vvgtest/ja/testovi_prihvacanja.java
@@ -9,57 +9,27 @@ public class testovi_prihvacanja implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @org.kie.api.definition.type.Label(value = "testPrihva\u0107anjaID")
-   private java.lang.Long id_test_prih;
-   @org.kie.api.definition.type.Label(value = "TestnaSkriptaID")
-   private java.lang.Long id_testne_sripte;
-   @org.kie.api.definition.type.Label(value = "ZahtjevID")
-   private java.lang.Long id_zahtjeva;
-   @org.kie.api.definition.type.Label(value = "naziv testa prihva\u0107anja")
+   @org.kie.api.definition.type.Label("testPrihva?anjaID")
+   private testovi_prihvacanja id_test_prih;
+   @org.kie.api.definition.type.Label("TestnaSkriptaID")
+   private Testne_sripte id_testne_sripte;
+   @org.kie.api.definition.type.Label("ZahtjevID")
+   private Zahtjevi id_zahtjeva;
+   @org.kie.api.definition.type.Label("naziv testa prihva?anja")
    private java.lang.String naziv;
-   @org.kie.api.definition.type.Label(value = "stabilna verzija aplikacija")
+   @org.kie.api.definition.type.Label("stabilna verzija aplikacija")
    private java.lang.String verzije_aplikacija;
-   @org.kie.api.definition.type.Label(value = "naziv aplikacije")
+   @org.kie.api.definition.type.Label("naziv aplikacije")
    private java.lang.String naziv_aplikacija;
-   @org.kie.api.definition.type.Label(value = "opis")
+   @org.kie.api.definition.type.Label("opis")
    private java.lang.String opis_testa_prihvacanja;
-   @org.kie.api.definition.type.Label(value = "izvjestaj o spremnosti za produkciju")
-   private java.lang.Long id_izvjestaja;
-   @org.kie.api.definition.type.Label(value = "korisnik")
-   private java.lang.Long id_korisnika;
+   @org.kie.api.definition.type.Label("izvjestaj o spremnosti za produkciju")
+   private Izvjestaji id_izvjestaja;
+   @org.kie.api.definition.type.Label("korisnik")
+   private Korisnici id_korisnika;
 
    public testovi_prihvacanja()
    {
-   }
-
-   public java.lang.Long getId_test_prih()
-   {
-      return this.id_test_prih;
-   }
-
-   public void setId_test_prih(java.lang.Long id_test_prih)
-   {
-      this.id_test_prih = id_test_prih;
-   }
-
-   public java.lang.Long getId_testne_sripte()
-   {
-      return this.id_testne_sripte;
-   }
-
-   public void setId_testne_sripte(java.lang.Long id_testne_sripte)
-   {
-      this.id_testne_sripte = id_testne_sripte;
-   }
-
-   public java.lang.Long getId_zahtjeva()
-   {
-      return this.id_zahtjeva;
-   }
-
-   public void setId_zahtjeva(java.lang.Long id_zahtjeva)
-   {
-      this.id_zahtjeva = id_zahtjeva;
    }
 
    public java.lang.String getNaziv()
@@ -103,32 +73,62 @@ public class testovi_prihvacanja implements java.io.Serializable
       this.opis_testa_prihvacanja = opis_testa_prihvacanja;
    }
 
-   public java.lang.Long getId_izvjestaja()
+   public vvgtest.ja.testovi_prihvacanja getId_test_prih()
+   {
+      return this.id_test_prih;
+   }
+
+   public void setId_test_prih(vvgtest.ja.testovi_prihvacanja id_test_prih)
+   {
+      this.id_test_prih = id_test_prih;
+   }
+
+   public vvgtest.ja.Testne_sripte getId_testne_sripte()
+   {
+      return this.id_testne_sripte;
+   }
+
+   public void setId_testne_sripte(vvgtest.ja.Testne_sripte id_testne_sripte)
+   {
+      this.id_testne_sripte = id_testne_sripte;
+   }
+
+   public vvgtest.ja.Zahtjevi getId_zahtjeva()
+   {
+      return this.id_zahtjeva;
+   }
+
+   public void setId_zahtjeva(vvgtest.ja.Zahtjevi id_zahtjeva)
+   {
+      this.id_zahtjeva = id_zahtjeva;
+   }
+
+   public vvgtest.ja.Izvjestaji getId_izvjestaja()
    {
       return this.id_izvjestaja;
    }
 
-   public void setId_izvjestaja(java.lang.Long id_izvjestaja)
+   public void setId_izvjestaja(vvgtest.ja.Izvjestaji id_izvjestaja)
    {
       this.id_izvjestaja = id_izvjestaja;
    }
 
-   public java.lang.Long getId_korisnika()
+   public vvgtest.ja.Korisnici getId_korisnika()
    {
       return this.id_korisnika;
    }
 
-   public void setId_korisnika(java.lang.Long id_korisnika)
+   public void setId_korisnika(vvgtest.ja.Korisnici id_korisnika)
    {
       this.id_korisnika = id_korisnika;
    }
 
-   public testovi_prihvacanja(java.lang.Long id_test_prih,
-         java.lang.Long id_testne_sripte, java.lang.Long id_zahtjeva,
-         java.lang.String naziv, java.lang.String verzije_aplikacija,
-         java.lang.String naziv_aplikacija,
+   public testovi_prihvacanja(vvgtest.ja.testovi_prihvacanja id_test_prih,
+         vvgtest.ja.Testne_sripte id_testne_sripte,
+         vvgtest.ja.Zahtjevi id_zahtjeva, java.lang.String naziv,
+         java.lang.String verzije_aplikacija, java.lang.String naziv_aplikacija,
          java.lang.String opis_testa_prihvacanja,
-         java.lang.Long id_izvjestaja, java.lang.Long id_korisnika)
+         vvgtest.ja.Izvjestaji id_izvjestaja, vvgtest.ja.Korisnici id_korisnika)
    {
       this.id_test_prih = id_test_prih;
       this.id_testne_sripte = id_testne_sripte;
