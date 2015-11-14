39471277f573298bfdb6374fe885d61c33fc5ada
admin <>
2015-11-14 15:00:21 +0100

    sfsfs {/ja/src/main/java/vvgtest/ja/Korisnici.java}

--- ja/src/main/java/vvgtest/ja/Korisnici.java
+++ ja/src/main/java/vvgtest/ja/Korisnici.java
@@ -20,11 +20,11 @@ public class Korisnici implements java.io.Serializable
    @org.kie.api.definition.type.Label("Lozinka")
    private java.lang.String lozinka;
 
-   @org.kie.api.definition.type.Label(value = "mail adresa korisnika")
+   @org.kie.api.definition.type.Label("mail adresa korisnika")
    private java.lang.Long mail_adresa;
 
-   @org.kie.api.definition.type.Label(value = "ovlast_korisnika")
-   private java.lang.String id_ovlasti;
+   @org.kie.api.definition.type.Label("ovlast_korisnika")
+   private ovlasti id_ovlasti;
 
    public Korisnici()
    {
@@ -90,12 +90,12 @@ public class Korisnici implements java.io.Serializable
       this.mail_adresa = mail_adresa;
    }
 
-   public java.lang.String getId_ovlasti()
+   public vvgtest.ja.ovlasti getId_ovlasti()
    {
       return this.id_ovlasti;
    }
 
-   public void setId_ovlasti(java.lang.String id_ovlasti)
+   public void setId_ovlasti(vvgtest.ja.ovlasti id_ovlasti)
    {
       this.id_ovlasti = id_ovlasti;
    }
@@ -103,7 +103,7 @@ public class Korisnici implements java.io.Serializable
    public Korisnici(java.lang.Long id_korisnika, java.lang.String ime_korisnik,
          java.lang.String prezime_korisnika, java.lang.String korisnicko_ime,
          java.lang.String lozinka, java.lang.Long mail_adresa,
-         java.lang.String id_ovlasti)
+         vvgtest.ja.ovlasti id_ovlasti)
    {
       this.id_korisnika = id_korisnika;
       this.ime_korisnik = ime_korisnik;
